61e31d2116477f071bc1ed99996f2c752e6fb2cc
SophiGreen <phiainfinity@gmail.com>
2025-05-11 06:05:43 +0000

    commit

--- ArrayMethods.java
+++ ArrayMethods.java
@@ -12,7 +12,40 @@
 *	Collection of useful methods for int arrays.
 *********************************************
 *	ALGORITHM:
-*	TODO: <Pseudocode for selection sort here>
+*	PSUEDOCODE:
+-start
+Descriptions
+  String:
+  -String result = "{ " + a[0];
+    for(int i = 1; i < a.length; i++) {
+      result += ", " + a[i];
+    }
+     result += " }";
+    return result;
+  Swap:
+  -int temp = array[a];
+    array[a] = array[b];
+    array[b] = temp;
+  Index of Minimum: 
+  -int minValue = array[startIndex];
+    int minIndex = startIndex;
+    for(int i = startIndex+1; i < array.length; i++) {
+      if(minValue > array[i]) {
+        minValue = array[i];
+        minIndex = i;
+      }
+    }
+    return minIndex;
+  Reverse:
+  -for(int i = 0; i < array.length/2; i++) {
+      ArrayMethods.swap(array, i, array.length-1-i);
+    }
+  Selection Sort:
+  -for(int i = 0; i < array.length-1; i++) {
+      int indexMinValue = ArrayMethods.indexOfMin(array, i);
+    ArrayMethods.swap(array, i, indexMinValue);
+    }
+
 *********************************************
 
 /* UML CLASS DIAGRAM:
@@ -28,29 +61,59 @@ ArrayMethods
 -----------------------------------------
 */
 
-public class ArrayMethods
+public class ArrayMethods 
+
 {
   /**DESCRIPTION: */
   public static String arrayString(int[] a)
   {
-    return ""; //STUB to keep compiler happy
+    String result = "{ " + a[0];
+
+   
+
+    for(int i = 1; i < a.length; i++) {
+      result += ", " + a[i];
+    }
+     result += " }";
+
+    return result;
   }
   
   /**DESCRIPTION: */
   public static void swap(int[] array, int a, int b)
   {
-  
+    int temp = array[a];
+    array[a] = array[b];
+    array[b] = temp;
   }
 	
 	/**DESCRIPTION: */
   public static int indexOfMin(int[] array, int startIndex)
+   
   {
-    return 0; //STUB to keep compiler happy
+   
+
+    int minValue = array[startIndex];
+    int minIndex = startIndex;
+
+    for(int i = startIndex+1; i < array.length; i++) {
+      if(minValue > array[i]) {
+        minValue = array[i];
+        minIndex = i;
+      }
+    }
+    return minIndex;
+
   }
+  
+   
 
 	/**DESCRIPTION: */
   public static void reverse(int[] array)
   {
+    for(int i = 0; i < array.length/2; i++) {
+      ArrayMethods.swap(array, i, array.length-1-i);
+    }
 
   }
 
@@ -58,6 +121,10 @@ public class ArrayMethods
   public static void selectionSort(int[] array)
   {
 
+    for(int i = 0; i < array.length-1; i++) {
+      int indexMinValue = ArrayMethods.indexOfMin(array, i);
+    ArrayMethods.swap(array, i, indexMinValue);
+    }
   }
 
 }
